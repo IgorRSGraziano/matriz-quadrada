d57fb73f0efba5cd282e9d94c0d59845f7b97184
IgorRSGraziano <igorrsgraziano@gmail.com>
2022-01-08 16:39:16 -0300

    add matrix generator function

--- script.js
+++ script.js
@@ -9,14 +9,26 @@ const formCheckbox = document.querySelector("#highlight-diagonals")
 formMain.addEventListener('submit', (e) => {
     // Impede que a página recarregada ao realizar um submit
     e.preventDefault()
+    const formBtn = document.querySelector("#form-main-submit")
 
-
-    formRows = document.querySelector("#row").value;
-    formCollums = document.querySelector("#collums").value
+    const formRows = document.querySelector("#form-main-rows").value;
+    const formCollums = document.querySelector("#form-main-collums").value
 
     const checkBoxField = document.querySelector('.form-checkbox-field')
 
-    formRows === formCollums ? checkBoxField.classList.toggle('hide') : checkBoxField.classList.add('hide')
+    //Cria uma matriz e se os valores forem iguais, vai exibir o próximo campo
+    if (formRows === formCollums) {
+        checkBoxField.classList.remove('hide')
+        generateMatrix(matrixTable, formCollums, formRows)
+        matrixTable.classList.remove('hide')
+    } else {
+        checkBoxField.classList.add('hide')
+        generateMatrix(matrixTable, formCollums, formRows)
+        matrixTable.classList.remove('hide')
+    }
+
+    //Impede reenvio do formulário
+    formBtn.disabled = true;
 })
 
 //Verifica se a checkbox está marcada, se estiver abrirá um novo campo para preencher as cores das diagonais.
