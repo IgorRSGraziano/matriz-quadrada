7ac9fdae7f2831f86617b0a9f964d254e0a8a738
IgorRSGraziano <igorrsgraziano@gmail.com>
2022-01-08 16:40:19 -0300

    add generateMatrix

--- script.js
+++ script.js
@@ -5,7 +5,55 @@ const formMain = document.querySelector(".main-form")
 const formCheckbox = document.querySelector("#highlight-diagonals")
 
 
-//Verifica se os valores de linha e coluna são iguais, se sim exibe a área do checkbox
+//Preenche com TR e TDS e gerando valores referente as linhas e colunas dela
+function generateMatrix(table, collums, rows) {
+    //Verifica se a quantidade de linhas é menor do que a do parâmetro, e cria até ter o mesmo valor
+    for (let indexRow = 0; indexRow < rows; indexRow++) {
+        table.insertRow(table.rows.lenght)
+        //Dentro da linha criada, irá criar a quantidade de colunas  de acordo com o parâmetro.
+        for (let indexCollum = 0; indexCollum < collums; indexCollum++) {
+            table.rows[indexRow].insertCell(indexCollum)
+            //Está pegando a array-like da linha e transformando em uma array e criando o conteúdo de texto dela de acordo com a linha e coluna que está inserida. Foi adicionado + 1 para o primeiro valor retornar como 1.
+            for (let collums = 0; collums < indexCollum + 1; collums++) {
+                Array.from(table.rows)[indexRow].cells[collums].innerText = `${indexRow + 1} ${collums + 1}`
+            }
+        }
+    }
+}
+
+//As diagonais da matriz tem o mesmo valor representando tanto a linha quanto a coluna. Irá verificar quais são diagonais, as diagonais receberão uma cor primária selecionada no formulário, e as não diagonais receberação a cor secundaria
+function addTagDiagonalsMatrix(table) {
+    for (let indexRow = 0; indexRow < table.rows.length; indexRow++) {
+        Array.from(table.rows[indexRow].cells).forEach((e) => {
+            if ((e.innerHTML.split(' ')[0] == e.innerHTML.split(' ')[1])) {
+                e.classList.add('diagonals')
+            } else {
+                e.classList.add('no-diagonals')
+            }
+        })
+    }
+}
+
+//Usado como callback. Verifica qual cor foi editado, e atualiza a tag de acordo.
+function updateBackgroundColor(e) {
+    if (e.target.id === 'form-color-main') {
+        document.querySelectorAll('.diagonals').forEach((el) => {
+            el.style.backgroundColor = e.target.value
+        })
+    } else if (e.target.id === 'form-color-second') {
+        document.querySelectorAll('.no-diagonals').forEach((el) => {
+            el.style.backgroundColor = e.target.value
+        })
+    }
+}
+
+
+
+//Execução
+
+
+
+//Verifica se os valores de linha e coluna são iguais, se sim exibe a área do checkbox, se não gera a matriz
 formMain.addEventListener('submit', (e) => {
     // Impede que a página recarregada ao realizar um submit
     e.preventDefault()
